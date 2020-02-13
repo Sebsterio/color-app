c11622fa43028a95b358f99152c899cc7df3178d
Sebsterio <sebastian.rosloniec@gmail.com>
2020-02-13 15:13:14 +0100

    add Palette component

---
+++ src/Palette.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Palette extends Component {
+	render() {
+		return (
+			<div className="Palette">
+				{/* Nav here */}
+				<div className="Pallete-colors">{/* color boxes here */}</div>
+				{/* footer here */}
+			</div>
+		);
+	}
+}
+
+export default Palette;
