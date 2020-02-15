15987a6f84dde8774ae096f180102fc66450741a
Sebsterio <sebastian.rosloniec@gmail.com>
2020-02-15 14:04:56 +0100

    add NewPaletteForm

--- src/App.js
+++ src/App.js
@@ -3,51 +3,53 @@ import { Route, Switch } from "react-router-dom";
 import Palette from "./Palette";
 import PaletteList from "./PaletteList";
 import SingleColorPalette from "./SingleColorPalette";
+import NewPaletteForm from "./NewPaletteForm";
 import seedColors from "./seedColors";
 import { generatePalette } from "./colorHelpers";
 
 class App extends Component {
-  findPalette(id) {
-    return seedColors.find(function(palette) {
-      return palette.id === id;
-    });
-  }
-  render() {
-    return (
-      <Switch>
-        <Route
-          exact
-          path='/palette/:paletteId/:colorId'
-          render={routeProps => (
-            <SingleColorPalette
-              colorId={routeProps.match.params.colorId}
-              palette={generatePalette(
-                this.findPalette(routeProps.match.params.paletteId)
-              )}
-            />
-          )}
-        />
-        <Route
-          exact
-          path='/'
-          render={routeProps => (
-            <PaletteList palettes={seedColors} {...routeProps} />
-          )}
-        />
-        <Route
-          exact
-          path='/palette/:id'
-          render={routeProps => (
-            <Palette
-              palette={generatePalette(
-                this.findPalette(routeProps.match.params.id)
-              )}
-            />
-          )}
-        />
-      </Switch>
-    );
-  }
+	findPalette(id) {
+		return seedColors.find(function(palette) {
+			return palette.id === id;
+		});
+	}
+	render() {
+		return (
+			<Switch>
+				<Route exact path="/palette/new" render={() => <NewPaletteForm />} />
+				<Route
+					exact
+					path="/palette/:paletteId/:colorId"
+					render={routeProps => (
+						<SingleColorPalette
+							colorId={routeProps.match.params.colorId}
+							palette={generatePalette(
+								this.findPalette(routeProps.match.params.paletteId)
+							)}
+						/>
+					)}
+				/>
+				<Route
+					exact
+					path="/"
+					render={routeProps => (
+						<PaletteList palettes={seedColors} {...routeProps} />
+					)}
+				/>
+				<Route
+					exact
+					path="/palette/:id"
+					render={routeProps => (
+						<Palette
+							palette={generatePalette(
+								this.findPalette(routeProps.match.params.id)
+							)}
+						/>
+					)}
+				/>
+			</Switch>
+		);
+	}
 }
 
 export default App;
