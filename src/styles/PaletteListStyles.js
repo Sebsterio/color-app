5a0a873216f5462018ab1712e995a7373d731e38
Sebsterio <sebastian.rosloniec@gmail.com>
2020-02-16 17:31:47 +0100

    make ressponsive

--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -1,3 +1,4 @@
+import sizes from "./sizes";
 export default {
 	root: {
 		backgroundColor: "blue",
@@ -7,16 +8,19 @@ export default {
 		justifyContent: "center"
 	},
 	container: {
-		width: "100%",
-		maxWidth: "800px",
-		padding: "0 1rem",
+		width: "50%",
 		display: "flex",
 		alignItems: "flex-start",
 		flexDirection: "column",
-		flexWrap: "wrap"
+		flexWrap: "wrap",
+		[sizes.down("xl")]: {
+			width: "80%"
+		},
+		[sizes.down("xs")]: {
+			width: "75%"
+		}
 	},
 	nav: {
-		fontFamily: "Roboto",
 		display: "flex",
 		width: "100%",
 		justifyContent: "space-between",
@@ -31,6 +35,13 @@ export default {
 		width: "100%",
 		display: "grid",
 		gridTemplateColumns: "repeat(3, 30%)",
-		gridGap: "5%"
+		gridGap: "2.5rem",
+		[sizes.down("md")]: {
+			gridTemplateColumns: "repeat(2, 50%)"
+		},
+		[sizes.down("xs")]: {
+			gridTemplateColumns: "repeat(1, 100%)",
+			gridGap: "1rem"
+		}
 	}
 };
