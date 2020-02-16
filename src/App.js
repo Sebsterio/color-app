3bed1f8fa51cab60cfed638af0ced0df33f78f74
Sebsterio <sebastian.rosloniec@gmail.com>
2020-02-16 14:48:17 +0100

    extract style to separate files; save palettes to  localStorage

--- src/App.js
+++ src/App.js
@@ -10,19 +10,32 @@ import { generatePalette } from "./colorHelpers";
 class App extends Component {
 	constructor(props) {
 		super(props);
+		const savedPalettes = JSON.parse(window.localStorage.getItem("palettes"));
 		this.state = {
-			palettes: seedColors
+			palettes: savedPalettes || seedColors
 		};
 		this.findPalette = this.findPalette.bind(this);
 		this.savePalette = this.savePalette.bind(this);
+		this.syncLocalStorage = this.syncLocalStorage.bind(this);
 	}
+
+	syncLocalStorage() {
+		window.localStorage.setItem(
+			"palettes",
+			JSON.stringify(this.state.palettes)
+		);
+	}
+
 	findPalette(id) {
 		return this.state.palettes.find(function(palette) {
 			return palette.id === id;
 		});
 	}
 	savePalette(newPalette) {
-		this.setState({ palettes: [...this.state.palettes, newPalette] });
+		this.setState(
+			{ palettes: [...this.state.palettes, newPalette] },
+			this.syncLocalStorage
+		);
 	}
 	render() {
 		return (
--- src/PaletteFormNav.jsx
+++ src/PaletteFormNav.jsx
@@ -10,50 +10,12 @@ import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 import Button from "@material-ui/core/Button";
-import { ValidatorForm, TextValidator } from "react-material-ui-form-validator";
-
-const drawerWidth = 400;
-const styles = theme => ({
-	root: {
-		display: "flex"
-	},
-	appBar: {
-		transition: theme.transitions.create(["margin", "width"], {
-			easing: theme.transitions.easing.sharp,
-			duration: theme.transitions.duration.leavingScreen
-		}),
-		flexDirection: "row",
-		justifyContent: "space-between",
-		alignItems: "center",
-		height: "64px"
-	},
-	appBarShift: {
-		width: `calc(100% - ${drawerWidth}px)`,
-		marginLeft: drawerWidth,
-		transition: theme.transitions.create(["margin", "width"], {
-			easing: theme.transitions.easing.easeOut,
-			duration: theme.transitions.duration.enteringScreen
-		})
-	},
-	menuButton: {
-		marginLeft: 12,
-		marginRight: 20
-	},
-	navBtns: {
-		marginRight: "1rem",
-		"& a": {
-			textDecoration: "none"
-		}
-	},
-	button: {
-		margin: "0 0.5rem"
-	}
-});
+import styles from "./styles/PaletteFormNavStyles";
 
 class PaletteFormNav extends Component {
 	constructor(props) {
 		super(props);
-		this.state = { newPaletteName: "", formShowing: false };
+		this.state = { formShowing: false };
 		this.handleChange = this.handleChange.bind(this);
 		this.showForm = this.showForm.bind(this);
 		this.hideForm = this.hideForm.bind(this);
@@ -72,7 +34,6 @@ class PaletteFormNav extends Component {
 	}
 	render() {
 		const { classes, open, palettes, handleSubmit } = this.props;
-		const { newPaletteName } = this.state;
 		return (
 			<div className={classes.root}>
 				<CssBaseline />
